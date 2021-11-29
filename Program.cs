4aa867b13c69e03068969ed2037d3e9e4a0510e8
SurroundedByGamers <95206742+SurroundedByGamers@users.noreply.github.com>
2021-11-29 12:23:10 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("A SHARP CALCULATOR");
+Console.ForegroundColor = ConsoleColor.White;
+
+Console.Write("What Is The First Number - ");
+double number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("What Is The Operator Of The Operation You Wanna Do? (+, -, *, /) - ");
+String mathOperator = Console.ReadLine();
+
+Console.Write("What Is The Second Number - ");
+double number2 = Convert.ToInt32(Console.ReadLine());
+
+switch (mathOperator)
+{
+    case "+":
+        Console.WriteLine(number1 + " + " + number2 + " = " + (number1 + number2));
+        break;
+    case "-":
+        Console.WriteLine(number1 + " - " + number2 + " = " + (number1 - number2));
+        break;
+    case "*":
+        Console.WriteLine(number1 + " * " + number2 + " = " + (number1 * number2));
+        break;
+    case "/":
+        Console.WriteLine(number1 + " / " + number2 + " = " + (number1 / number2));
+        break;
+    default:
+        Console.WriteLine("Hmmm.. I Did Not Know About The Operator '" + mathOperator + "'");
+        break;
+}
